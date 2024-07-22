abb22e2d691c99e3cee51fdc14c073416ddf7db3
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-07-22 14:43:14 +0200

    created chair class

---
+++ Game-framework/Chair.cpp
@@ -0,0 +1,37 @@
+#include "Chair.h"
+
+#include "Application.h"
+
+bool CChair::Create(void)
+{
+	m_pTexture = m_pApplication->GetTextureHandler().CreateTexture("Assets/Textures/chair.png");
+	if (!m_pTexture)
+		return false;
+
+	int textureWidth = 0;
+	int textureHeight = 0;
+	SDL_QueryTexture(m_pTexture, nullptr, nullptr, &textureWidth, &textureHeight);
+
+	m_Size = { (float)(textureWidth * 0.7f), (float)(textureHeight * 0.7f) };
+
+	m_Rectangle = { 100.0f, m_pApplication->GetWindow().GetSize().y - m_Size.y, m_Size.x, m_Size.y };
+
+	m_Collider = { m_Rectangle.x, m_Rectangle.y, m_Size.x, 32.0f };
+
+	return true;
+}
+
+void CChair::Render(void)
+{
+	m_Collider = { m_Rectangle.x + 10.0f, m_Rectangle.y + 10.0f, m_Size.x - 20.0f, 25.0f };
+
+	m_pApplication->GetTextureHandler().RenderTexture(m_pTexture, { m_Rectangle.x, m_pApplication->GetWindow().GetSize().y - m_Size.y }, nullptr, &m_Size);
+}
+
+void CChair::RenderDebug(void)
+{
+	SDL_Renderer* renderer = m_pApplication->GetWindow().GetRenderer();
+
+	SDL_SetRenderDrawColor(renderer, 200, 0, 0, 255);
+	SDL_RenderDrawRectF(renderer, &m_Collider);
+}
--- Game-framework/Chair.h
+++ Game-framework/Chair.h
@@ -1,5 +1,25 @@
 #pragma once
-class Chair
+
+#include "GameObjects/GameObject.h"
+
+#include <SDL.h>
+#include <vector>
+
+class CChair final : public CGameObject
 {
+public:
+
+	CChair(void) {}
+	CChair(CApplication* application) : CGameObject(application) {}
+	~CChair(void) {}
+
+	virtual bool Create(void) override;
+	virtual void Render(void) override;
+	virtual void RenderDebug(void) override;
+
+private:
+
+	SDL_FPoint m_Size = { 0.0f, 0.0f };
+
 };
 
