fc13a15efe16c1a75326f3259a369e15d0136ef3
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-07-23 16:14:22 +0200

    create upload spider texture

--- Game-framework/Spider.cpp
+++ Game-framework/Spider.cpp
@@ -1,14 +1,35 @@
 #include "Spider.h"
 
+#include "Application.h"
+
 bool CSpider::Create(void)
 {
-    return false;
+	m_pTexture = m_pApplication->GetTextureHandler().CreateTexture("Assets/Textures/spiderTest.png");
+	if (!m_pTexture)
+		return false;
+
+	int textureWidth = 0;
+	int textureHeight = 0;
+	SDL_QueryTexture(m_pTexture, nullptr, nullptr, &textureWidth, &textureHeight);
+
+	m_Size = { (float)(textureWidth * 0.7f), (float)(textureHeight * 0.7f) };
+
+	m_Rectangle = { 900.0f, m_pApplication->GetWindow().GetSize().y - m_Size.y, m_Size.x, m_Size.y };
+
+	m_Collider = { m_Rectangle.x + 5.0f, m_Rectangle.y + 85.0f, m_Size.x - 10.0f, 20.0f };
+
+	return true;
 }
 
 void CSpider::Render(void)
 {
+	m_pApplication->GetTextureHandler().RenderTexture(m_pTexture, { m_Rectangle.x, m_pApplication->GetWindow().GetSize().y - m_Size.y }, nullptr, &m_Size);
 }
 
 void CSpider::RenderDebug(void)
 {
+	SDL_Renderer* renderer = m_pApplication->GetWindow().GetRenderer();
+
+	SDL_SetRenderDrawColor(renderer, 200, 0, 0, 255);
+	SDL_RenderDrawRectF(renderer, &m_Collider);
 }
