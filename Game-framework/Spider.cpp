689922c1113ad0bcc320a62ce02a401948298cb9
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-07-23 16:08:07 +0200

    added the fuctions for the spider.h

--- Game-framework/Spider.cpp
+++ Game-framework/Spider.cpp
@@ -1,1 +1,14 @@
 #include "Spider.h"
+
+bool CSpider::Create(void)
+{
+    return false;
+}
+
+void CSpider::Render(void)
+{
+}
+
+void CSpider::RenderDebug(void)
+{
+}
--- Game-framework/Spider.h
+++ Game-framework/Spider.h
@@ -1,5 +1,24 @@
 #pragma once
-class Spider
+
+#include "GameObjects/GameObject.h"
+
+#include <SDL.h>
+#include <vector>
+
+class CSpider final : public CGameObject
 {
+public:
+
+	CSpider(void) {}
+	CSpider(CApplication* application) : CGameObject(application) {}
+		~CSpider(void) {}
+
+	virtual bool Create(void) override;
+	virtual void Render(void) override;
+	virtual void RenderDebug(void) override;
+
+private:
+
+	SDL_FPoint m_Size = { 0.0f, 0.0f };
 };
 
