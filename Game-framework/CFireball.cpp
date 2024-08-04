df97a577b289336caa7afb1b45e2008dd1f75bf9
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-08-04 22:48:27 +0200

    added update function as well as setter and getter IsActive

--- Game-framework/CFireball.cpp
+++ Game-framework/CFireball.cpp
@@ -1,1 +1,6 @@
 #include "CFireball.h"
+
+void CFireball::Update(const float deltaTime)
+{
+	m_Rectangle.x += 4.0f * deltaTime;
+}
--- Game-framework/CFireball.h
+++ Game-framework/CFireball.h
@@ -1,5 +1,20 @@
 #pragma once
-class CFireball
+
+#include "GameObjects/GameObject.h"
+
+#include <SDL.h>
+
+class CFireball final : public CGameObject
 {
+public:
+	 bool GetIsActive() const  { return m_IsActive; }
+
+	 void SetIsActive(const bool isActive)  { m_IsActive = isActive; }
+
+	 virtual void	Update(const float deltaTime);
+
+private:
+
+	bool m_IsActive = false;
 };
 
