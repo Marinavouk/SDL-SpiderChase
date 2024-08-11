249b1a6f40060afa956d2ff79035ed2232630d38
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-08-11 13:31:45 +0200

    created EndRoundState.h and cpp

---
+++ Game-framework/EndRoundState.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "States/State.h"
+#include "Utilities/Texture.h"
+#include "Handlers/TextureHandler.h"
+
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+
+class EndRoundState final : public CState
+{
+
+public:
+
+};
+
